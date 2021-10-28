7a0aa5aae3ef2a01a786161041cc17fb9175c4a8
Claus Rüffler <claus.rueffler@posteo.net>
2021-10-28 21:55:16 +0200

    added logical subsetting

---
+++ exercises.R
@@ -0,0 +1,41 @@
+# first exercises
+# a
+z <- seq(2, 100, 2)
+# b
+z <- 12*1:8
+z
+z %in% y
+x <-y %in% z
+q <- which(x==TRUE)*2
+q
+length(q)
+# trick
+z %% 12 == 0
+z[z %% 12 == 0]
+# note: logical subsetting
+c(1, 2, 3)[c(TRUE, TRUE, FALSE)]
+# c
+sum(z)
+# d
+sum(z) == 51*50
+# e
+z[5]*z[10]*z[15]
+# f
+y <- 3*(1:10)
+y[y %in% z]
+# note
+x <- y %in% z
+y[x] 
+# g
+seq(2, 100, by = 2)== (1:50) * 2
+# h
+z^2
+
+## second exercises
+# a)
+data(trees)
+mean(trees$Height)
+# b
+mean(trees[trees$Height > 75, ]$Girth)
+# c
+max(trees[15 < trees$Volume & 35 > trees$Volume, ]$Height)
