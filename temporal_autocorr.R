71ca9780074a9a71055e31847df41dcd5048d64a
Claus Rüffler <claus.rueffler@posteo.net>
2022-10-30 11:19:03 +0100

    updates Console tab

---
+++ temporal_autocorr.R
@@ -0,0 +1,19 @@
+a <- 0.2
+time <- 100
+
+pos.auto.matrix <- matrix(c(1-a, a, a, 1-a), 2, 2, byrow = TRUE)
+
+results <- matrix(c(1,0))
+for (i in 1:time) {
+  pnext <- pos.auto.matrix %*% results[, i]
+  results <- cbind(results, c(rbinom(1, 1, pnext), 1-rbinom(1, 1, pnext)))
+}
+
+rbinom(1, 1, 0.5)
+results
+ncol(results)
+
+matplot(1:ncol(results), t(results))
+line(results[2,])
+plot(1:ncol(results), results[1,])
+
