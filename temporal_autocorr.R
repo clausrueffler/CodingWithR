f358683cad5f3ffd08ad48bf2158b42833e2697b
Claus Rüffler <claus.rueffler@posteo.net>
2023-11-06 19:36:28 +0100

    various updates

--- temporal_autocorr.R
+++ temporal_autocorr.R
@@ -1,4 +1,4 @@
-a <- 0.2
+a <- 0.3
 time <- 100
 
 pos.auto.matrix <- matrix(c(1-a, a, a, 1-a), 2, 2, byrow = TRUE)
@@ -6,7 +6,8 @@ pos.auto.matrix <- matrix(c(1-a, a, a, 1-a), 2, 2, byrow = TRUE)
 results <- matrix(c(1,0))
 for (i in 1:time) {
   pnext <- pos.auto.matrix %*% results[, i]
-  results <- cbind(results, c(rbinom(1, 1, pnext), 1-rbinom(1, 1, pnext)))
+  state1 <- c(rbinom(1, 1, pnext[1]))
+  results <- cbind(results, c(state1, 1 - state1))
 }
 
 rbinom(1, 1, 0.5)
@@ -16,4 +17,3 @@ ncol(results)
 matplot(1:ncol(results), t(results))
 line(results[2,])
 plot(1:ncol(results), results[1,])
-
